138c5d96634f3125e957a07cfdc43ac0fd453d6d
Anirudh Katti <kattianirudh@gmail.com>
2022-06-16 15:59:47 -0500

    Again

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from ast import keyword
 import math
-from colorama import Cursor
 from flask import Flask, render_template, request, jsonify
 # from flask_cors import CORS
 # from flask_sqlalchemy import SQLAlchemy
